54321b3c018f65b6f405dc8439685217768c9606
GeonWoo Lee <2dubu.dev@gmail.com>
2024-07-12 20:23:16 +0900

    🎉 :: tada
    inital project file

---
+++ HARUCHI/HARUCHI/HARUCHIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HARUCHIApp.swift
+//  HARUCHI
+//
+//  Created by 이건우 on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HARUCHIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
