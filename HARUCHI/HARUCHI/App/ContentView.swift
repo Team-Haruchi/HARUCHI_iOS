979fd21852648651b2d6af3acec0b660ebbab5d4
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-07-17 00:35:37 +0900

    :sparkles: ::  Add TabBar to ContentView

--- HARUCHI/HARUCHI/App/ContentView.swift
+++ HARUCHI/HARUCHI/App/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("홈화면")
+                .tabItem {
+                    Image("Home")
+                    Text("홈")
+                }
+            Text("예산창")
+                .tabItem {
+                    Image("Check Book")
+                    Text("예산")
+                }
+            Text("사이클 분석")
+                .tabItem {
+                    Image("Graph")
+                    Text("분석")
+                }
+            Text("메뉴")
+                .tabItem {
+                    Image("Menu")
+                    Text("더보기")
+                }
         }
-        .padding()
+        
+        .onAppear {
+            UITabBar.appearance().unselectedItemTintColor = UIColor(Color.haruchiGrey81)
+        }
+        .accentColor(.black)
     }
 }
 
