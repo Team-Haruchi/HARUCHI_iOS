d3f9bcb2c3ef33eb04744c07b5bd45d140590917
GeonWoo Lee <2dubu.dev@gmail.com>
2024-07-22 21:26:15 +0900

    🍱 :: Add icons
    checkBox, checkMark

---
+++ HARUCHI/HARUCHI/Components/GrayLine.swift
@@ -0,0 +1,18 @@
+//
+//  GrayLine.swift
+//  HARUCHI
+//
+//  Created by 이건우 on 7/21/24.
+//
+
+import SwiftUI
+
+struct GrayLine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GrayLine()
+}
