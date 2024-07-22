a8228ff350b3279bcf4337baa202c5510aad4b9f
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-07-22 17:12:11 +0900

    :sparkles: :: Add nickname validation rules

--- HARUCHI/HARUCHI/App/Onboarding/OnboardingNicknameView.swift
+++ HARUCHI/HARUCHI/App/Onboarding/OnboardingNicknameView.swift
@@ -6,38 +6,60 @@
 //
 
 import SwiftUI
+import Combine
+
+enum TextLengthStatus {
+    case valid
+    case invalid
+    case `default`
+}
 
 struct OnboardingNicknameView: View {
     @State var text = ""
+    @State private var limitLength: TextLengthStatus = .default
+    private let maxLength = 5
 
     var body: some View {
-        VStack(alignment: .leading) {
-            VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 Text("가입을 축하드려요!")
-                     
                 Text("어떻게 불러드리면 될까요?")
             }
             .font(.haruchi(.h1))
             .foregroundColor(Color.black)
-            .padding(.bottom, 14)
-            .frame(width: 253, height: 64)
+            .frame(width: 253, height: 64, alignment: .leading)
             .padding(.top, 124)
-            .padding(.leading, 4)
-
-            HStack(alignment: .center) {
-                TextField("(한글) 5글자 내로 입력해주세요.", text: $text)
-                    .font(.haruchi(.h2))
-                    .foregroundColor(.gray6)
-                    .keyboardType(.default)
-                    .multilineTextAlignment(.leading)
-                    .padding(.vertical, 5)
-            }
-            .padding(.top, 333)
-            .padding(.leading, 4)
-            .padding(.trailing, 4)
-            .frame(width: 345, height: 31)
+            .padding(.leading, 24)
             
-            Spacer()
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .center) {
+                    TextField("(한글) 5글자 내로 입력해주세요.", text: $text)
+                        .font(.haruchi(.h2))
+                        .foregroundColor(limitLength == .invalid ? Color.black : Color.red)
+                        .keyboardType(.default)
+                        .multilineTextAlignment(.leading)
+                        .padding(.vertical, 5)
+                    
+                        .onChange(of: text) { newValue in
+                            self.validateAndLimitText()
+                        }
+                }
+                .padding(.leading, 24)
+                .frame(width: 344, height: 24)
+                
+                HStack {
+                    Text(limitLength == .invalid ? "올바르지 않은 형식입니다." : "")
+                        .font(.haruchi(.caption3))
+                        .foregroundColor(Color.red)
+                    Spacer()
+                    Text("\(text.count)/\(maxLength)")
+                        .font(.haruchi(.caption3))
+                        .foregroundColor(text.count > maxLength ? Color.red : Color.black)
+                }
+                .padding(.top, 4)
+                .padding(.leading, 24)
+            }
+            .padding(.top, 183)
         }
         .ignoresSafeArea(.keyboard)
         
@@ -50,8 +72,29 @@ struct OnboardingNicknameView: View {
             }
         }
     }
-}
+    
+    private func validateAndLimitText() {
+        if text.count > maxLength {
+            text = String(text.prefix(maxLength))
+            limitLength = .valid
+        } else if text.isEmpty || !isKoreanOrEnglish(text) {
+            limitLength = .invalid
+        } else {
+            limitLength = .valid
+        }
+    }
 
+    private func isKoreanOrEnglish(_ input: String) -> Bool {
+        let pattern = "^[가-힣a-zA-Z\\s]*$"
+        if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
+            let range = NSRange(location: 0, length: input.utf16.count)
+            if regex.firstMatch(in: input, options: [], range: range) != nil {
+                return true
+            }
+        }
+        return false
+    }
+}
 
 #Preview {
     OnboardingNicknameView()
