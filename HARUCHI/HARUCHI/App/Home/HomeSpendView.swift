48fbdb1ab38c4880fb4ff25ca81c219df4c4fbc8
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-07-26 17:48:20 +0900

    ✨ :: HomeSpendView category
    수입/지출 카테고리 분류 추가

--- HARUCHI/HARUCHI/App/Home/HomeSpendView.swift
+++ HARUCHI/HARUCHI/App/Home/HomeSpendView.swift
@@ -9,46 +9,84 @@ import SwiftUI
 
 struct HomeSpendView: View {
     @State private var money = ""
-    
+
+    var category = ["수입", "지출"]
+    @State private var selectedCategory = ""
     
     var body: some View {
         VStack(spacing: 0) {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("남은 일정과 금액")
-                    .font(.haruchi(.h2))
-                    .foregroundColor(Color.black)
-                
-                Text("D-14 / 230000원")
-                    .font(.haruchi(.caption3))
-                    .foregroundColor(Color.gray5)
-                
-                TextField("하루치 20000원", text: $money)
-                    .font(.haruchi(.h2))
-                    .foregroundColor(Color.gray5)
-                    .keyboardType(.numberPad)
-                    .multilineTextAlignment(.leading)
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("남은 일정과 금액")
+                        .font(.haruchi(.h2))
+                        .foregroundColor(Color.black)
+                        .padding(.bottom, 10)
+                    
+                    Text("D-14 / 230000원")
+                        .font(.haruchi(.caption3))
+                        .foregroundColor(Color.gray5)
+                        .padding(.bottom, 25)
+                    
+                    TextField("하루치 20000원", text: $money)
+                        .font(.haruchi(.h2))
+                        .foregroundColor(Color.black)
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                }
+                Spacer()
             }
-            .padding(.leading, 24)
+            .padding(.horizontal, 24)
             
             HStack {
                 Text("분류")
+                    .foregroundColor(Color.gray5)
+                    .font(.haruchi(.body_r16))
                 
-                Text("수입 지출")
+                Spacer()
+                
+                ForEach(category, id: \.self) { category in
+                    Text(category)
+                        .background(Color.clear)
+                        .foregroundColor(selectedCategory == category ? Color.black : Color.gray5)
+                        .font(selectedCategory == category ? .haruchi(.body_sb16) : .haruchi(.body_r16))
+                        .onTapGesture {
+                            selectedCategory = category
+                        }
+                    
+                    // 수입 지출 중간 간격
+                    if category != self.category.last {
+                        Spacer().frame(width: 22)
+                    }
+                }
+                .background(Color.clear)
+                .clipShape(Capsule())
             }
+            .frame(width: 345, height: 45)
+            .padding(.horizontal, 24)
+            .padding(.top, 40)
             
             HStack {
-                Text("분류")
+                Text("카테고리")
                 
-                Text("수입 지출")
+                Spacer()
+                
+                Text("미분류")
+                Image(systemName: "chevron.right")
             }
+            .font(.haruchi(.body_r16))
+            .foregroundColor(Color.gray5)
+            .frame(width: 345, height: 45)
+            .padding(.horizontal, 24)
         }
-        .toolbar {
-            ToolbarItemGroup(placement: .keyboard) {
-                KeypadButton(
-                    text: "저장하기", enable: !money.isEmpty, action: { print("버튼 눌림 ㅇㅇ") }
-                )
+        Spacer()
+        
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    KeypadButton(
+                        text: "저장하기", enable: !money.isEmpty, action: { print("버튼 눌림 ㅇㅇ") }
+                    )
+                }
             }
-        }
     }
 }
 
