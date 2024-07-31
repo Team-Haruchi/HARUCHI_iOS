84fdaab0baac5fa7251991b1c1ad251120ec4d9f
이슬기 <106726904+leeseulgi0208@users.noreply.github.com>
2024-07-31 17:32:19 +0900

    Revert "[Feat] Calendar"

--- HARUCHI/HARUCHI/App/Calendar/CalendarModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-// 날짜별 예산 데이터
-struct CalendarModel: Identifiable {
-    let id = UUID()    // 각 객체를 고유하게 식별하기 위한 UUID
-    let date: Date     // 예산이 적용되는 날짜
-    let budget: Double // 해당 날짜의 예산 금액
-}
