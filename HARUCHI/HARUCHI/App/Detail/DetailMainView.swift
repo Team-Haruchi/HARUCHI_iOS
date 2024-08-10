ad24dce8e9ee35917b06cfe4c6a72399ebca4e6b
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2024-08-11 04:57:49 +0900

    ✨ Membership Information
    회원 정보 UI 만들고 더보기 창이라 네비게이션 연결

--- HARUCHI/HARUCHI/App/Detail/DetailMainView.swift
+++ HARUCHI/HARUCHI/App/Detail/DetailMainView.swift
@@ -2,73 +2,76 @@ import SwiftUI
 
 struct DetailMainView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 30){
-            HStack{
-                Text("앱 정보")
-                    .padding(.top, 30)
-                    .font(.haruchi(.h2))
+        NavigationStack{
+            VStack(alignment: .leading, spacing: 30){
+                HStack{
+                    Text("앱 정보")
+                        .padding(.top, 30)
+                        .font(.haruchi(.h2))
+                    Spacer()
+                }
+                
+                NavigationLink(destination: MemberInfo()) {
+                    HStack {
+                        Text("회원정보")
+                            .font(.haruchi(.body_r16))
+                            .foregroundStyle(.black)
+                        Spacer()
+                        Image("chevron.right")
+                            .resizable()
+                            .frame(width: 6, height: 13)
+                    }
+                }
+                .frame(width: 345, height: 45)
+                
+                Button(action: {}) {
+                    HStack{
+                        Text("버전정보")
+                            .font(.haruchi(.body_r16))
+                            .foregroundStyle(.black)
+                        Spacer()
+                        Image("chevron.right")
+                            .resizable()
+                            .frame(width: 6, height: 13)
+                    }
+                }.frame(width: 345, height: 45)
+                
+                Button(action: {}) {
+                    HStack{
+                        Text("친구에게 앱 공유하기")
+                            .font(.haruchi(.body_r16))
+                            .foregroundStyle(.black)
+                        Spacer()
+                        Image("chevron.right")
+                            .resizable()
+                            .frame(width: 6, height: 13)
+                    }
+                }.frame(width: 345, height: 45)
+                
+                HStack{
+                    Text("계정")
+                        .font(.haruchi(.body_m14))
+                        .foregroundStyle(Color.gray5)
+                    Spacer()
+                }.frame(height: 50)
+                    .padding(.top, 70)
+                
+                Button(action: {}) {
+                    HStack{
+                        Text("탈퇴")
+                            .font(.haruchi(.body_r16))
+                            .foregroundStyle(.black)
+                        Spacer()
+                        Image("chevron.right")
+                            .resizable()
+                            .frame(width: 6, height: 13)
+                    }
+                }.frame(width: 345, height: 45)
+                    .padding(.top, -10)
+                
                 Spacer()
-            }
-            
-            Button(action: {}) {
-                HStack{
-                    Text("회원정보")
-                        .font(.haruchi(.body_r16))
-                        .foregroundStyle(.black)
-                    Spacer()
-                    Image("chevron.right")
-                        .resizable()
-                        .frame(width: 6, height: 13)
-                }
-            }.frame(width: 345, height: 45)
-            
-            Button(action: {}) {
-                HStack{
-                    Text("버전정보")
-                        .font(.haruchi(.body_r16))
-                        .foregroundStyle(.black)
-                    Spacer()
-                    Image("chevron.right")
-                        .resizable()
-                        .frame(width: 6, height: 13)
-                }
-            }.frame(width: 345, height: 45)
-            
-            Button(action: {}) {
-                HStack{
-                    Text("친구에게 앱 공유하기")
-                        .font(.haruchi(.body_r16))
-                        .foregroundStyle(.black)
-                    Spacer()
-                    Image("chevron.right")
-                        .resizable()
-                        .frame(width: 6, height: 13)
-                }
-            }.frame(width: 345, height: 45)
-            
-            HStack{
-                Text("계정")
-                    .font(.haruchi(.body_m14))
-                    .foregroundStyle(Color.gray5)
-                Spacer()
-            }.frame(height: 50)
-                .padding(.top, 70)
-            
-            Button(action: {}) {
-                HStack{
-                    Text("탈퇴")
-                        .font(.haruchi(.body_r16))
-                        .foregroundStyle(.black)
-                    Spacer()
-                    Image("chevron.right")
-                        .resizable()
-                        .frame(width: 6, height: 13)
-                }
-            }.frame(width: 345, height: 45)
-                .padding(.top, -10)
-            
-            Spacer()
-        }.padding(.horizontal, 24)
+            }.padding(.horizontal, 24)
+        }
     }
 }
 
---
+++ HARUCHI/HARUCHI/App/Detail/MemberInfo.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+struct MemberInfo: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15){
+            HStack{
+                Text("계정 정보")
+                    .padding(.top, 21)
+                    .font(.haruchi(.body_m16))
+                Spacer()
+            }
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text("닉네임")
+                    .font(.haruchi(.button14))
+                Text("저축왕")
+                    .font(.haruchi(.button12))
+                    .foregroundStyle(Color.gray7)
+            }.frame(height: 50)
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text("이메일")
+                    .font(.haruchi(.button14))
+                // AttributedString으로 이메일 텍스트를 명시적으로 정의
+                Text(AttributedString("haruchi@naver.com", attributes: .init([.foregroundColor: UIColor(Color.gray7)])))
+                    .font(.haruchi(.button12))
+            }.frame(height: 50)
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text("가입일")
+                    .font(.haruchi(.button14))
+                Text("2024.01.01")
+                    .font(.haruchi(.button12))
+                    .foregroundStyle(Color.gray7)
+            }.frame(height: 50)
+            
+            Spacer()
+        }.padding(.horizontal, 24)
+            .navigationBarBackButtonHidden(true)
+            .disableAutocorrection(true)
+            .backButtonStyle()
+            .toolbar {
+                ToolbarItem(placement: .principal) { // 가운데 제목 추가
+                    Text("계정/정보")
+                        .font(.haruchi(.h2))
+                        .foregroundColor(.black)
+                }
+            }
+    }
+}
+
+#Preview {
+    MemberInfo()
+}
