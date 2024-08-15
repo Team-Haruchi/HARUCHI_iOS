0560f76e1c661eaec2203d468bf6b764ce800e09
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-08-15 16:45:10 +0900

    ✨ :: IncomeResponse
    IncomeResponse 파일 추가

--- HARUCHI/HARUCHI/Network/Entity/IncomeResponse.swift
+++ HARUCHI/HARUCHI/Network/Entity/IncomeResponse.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct IncomeResponse<T: Decodable>: Decodable {
+    let isSuccess: Bool
+    let code: String
+    let message: String
+    let result: T
+}
+
+struct IncomeResult: Decodable {
+    let createdAt: String
+    let incomeID: Int
+}
