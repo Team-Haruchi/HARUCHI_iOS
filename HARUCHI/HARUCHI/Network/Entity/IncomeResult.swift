c7b882a52ef59e0c2b19e9f0035117257174495a
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-08-16 20:40:53 +0900

    ✨ :: IncomeResult

---
+++ HARUCHI/HARUCHI/Network/Entity/IncomeResult.swift
@@ -0,0 +1,13 @@
+//
+//  IncomeResult.swift
+//  HARUCHI
+//
+//  Created by 채리원 on 8/16/24.
+//
+
+import Foundation
+
+struct IncomeResult: Decodable {
+    let createdAt: String
+    let incomeID: Int
+}
