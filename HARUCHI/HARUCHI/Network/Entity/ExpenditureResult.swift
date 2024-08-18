cbc46dfeb61b3ec4a667c99720476c2937a33bc0
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-08-18 20:28:07 +0900

    ✨ :: ExpenditureResult
    ExpenditureResult 파일 추가

---
+++ HARUCHI/HARUCHI/Network/Entity/ExpenditureResult.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenditureResult.swift
+//  HARUCHI
+//
+//  Created by 채리원 on 8/18/24.
+//
+
+import Foundation
+
+struct ExpenditureResult: Decodable {
+    let createdAt: String
+    let expenditureResult: Int
+}
