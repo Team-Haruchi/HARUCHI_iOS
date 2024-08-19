b810ff614ca6dc14be4136abbb5b4bf60f6f7477
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-08-20 00:28:06 +0900

    ✨ :: CloseResult

---
+++ HARUCHI/HARUCHI/Network/Entity/CloseResult.swift
@@ -0,0 +1,15 @@
+//
+//  CloseResult.swift
+//  HARUCHI
+//
+//  Created by 채리원 on 8/20/24.
+//
+
+import Foundation
+
+struct CloseResult: Decodable {
+    let redistributionOption: String
+    let year: Int
+    let month: Int
+    let day: Int
+}
