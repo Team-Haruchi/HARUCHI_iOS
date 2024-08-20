4440669c58337e791da758a3d7c4527e294b050f
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-08-20 15:04:52 +0900

    ♻️ :: Refactoring Closing API

--- HARUCHI/HARUCHI/Network/Service/CloseService.swift
+++ HARUCHI/HARUCHI/Network/Service/CloseService.swift
@@ -17,14 +17,66 @@ class CloseService {
         self.provider = MoyaProvider<CloseAPI>(plugins: [AuthPlugin(token: token)])
     }
     
-    func requestIncome(
-        incomeAmount: Int,
-        category: String
+    func closeBudget(
+        redistributionOption: String,
+        year: Int,
+        month: Int,
+        day: Int
     ) -> AnyPublisher<Base<CloseResult>, Error> {
-        return provider.requestPublisher(.close(redistributionOption: redistributionOption, year: year, month: month, day: day))
+        return provider.requestPublisher(.closeBudget(redistributionOption: redistributionOption, year: year, month: month, day: day))
             .tryMap { response in
                 guard (200...299).contains(response.statusCode) else {
                     print("[CloseService] requestIncome() statusCode: \(response.statusCode)")
+                    throw MoyaError.statusCode(response)
+                }
+                return response.data
+            }
+            .decode(type: Base<CloseResult>.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func closeCheck (
+        year: Int,
+        month: Int,
+        day: Int
+    ) -> AnyPublisher<Base<CloseResult>, Error> {
+        return provider.requestPublisher(.closeCheck(year: year, month: month, day: day))
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    print("[CloseService] closeCheck() statusCode: \(response.statusCode)")
+                    throw MoyaError.statusCode(response)
+                }
+                return response.data
+            }
+            .decode(type: Base<CloseResult>.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func closeCheckLast() -> AnyPublisher<Base<CloseResult>, Error> {
+        return provider.requestPublisher(.closeCheckLast)
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    print("[CloseService] closeCheckLast() statusCode: \(response.statusCode)")
+                    throw MoyaError.statusCode(response)
+                }
+                return response.data
+            }
+            .decode(type: Base<CloseResult>.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func closeAmount(
+        year: Int,
+        month: Int,
+        day: Int
+    ) -> AnyPublisher<Base<CloseResult>, Error> {
+        return provider.requestPublisher(.closeAmount(year: year, month: month, day: day))
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    print("[CloseService] closeAmount() statusCode: \(response.statusCode)")
                     throw MoyaError.statusCode(response)
                 }
                 return response.data
